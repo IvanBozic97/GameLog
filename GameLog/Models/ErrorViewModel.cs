79239306c6215a8b1dc0370e3d5ad62f9ee0da45
Josip <Josip@DESKTOP-BVIUPJE>
2026-01-30 17:39:16 +0100

    Add project files.

---
+++ GameLog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameLog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
